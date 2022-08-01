13b3d8b416d596cf09a7d5439b977b103562ff3d
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-08-01 22:33:52 +0200

    change the outDir to the default get ride of the docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    outDir:'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
